b8199a1b114aaa963ab328ddeba43ac10bb90287
Kassy18 <kassandraandal601@gmail.com>
2025-09-01 18:11:47 +0800

    Update mini.js

--- mini.js
+++ mini.js
@@ -1,1 +1,7 @@
-
+document.getElementById("dogBtn").addEventListener("click", () => {
+      fetch("https://dog.ceo/api/breeds/image/random")
+        .then(response => response.json())
+        .then(data => {
+          document.getElementById("dogImage").src = data.message;
+        });
+    });
